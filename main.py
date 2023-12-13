aa28b214feb78c7d46183e6836558a848db50664
Roger Sullivan <sullivanrogerl@gmail.com>
2023-12-13 10:49:22 -0600

    made paddle and ball class set up paddle controls

---
+++ main.py
@@ -0,0 +1,28 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+screen = Screen()
+screen.screensize(800,600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350,0))
+ball = Ball((0, 0))
+
+screen.listen()
+
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down,"Down")
+
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down,"s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+screen.exitonclick()
